c6ead52b6358b6f43560c557c95bc0e38cab179b
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-07-07 11:41:05 +0600

    All router setup

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,13 +1,85 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Outlet, createBrowserRouter } from "react-router-dom";
 
 const routers = createBrowserRouter([
   {
     path: "/",
-    element: <>Main Layout</>,
+    element: (
+      <>
+        <h1>Home Layout</h1>
+        <Outlet></Outlet>
+      </>
+    ),
     children: [
       {
         path: "/",
         element: <>Home Page</>,
+      },
+      {
+        path: "/products",
+        element: <>Our Products Page</>,
+      },
+      {
+        path: "/products/:id",
+        element: <>Products Details Page</>,
+      },
+      {
+        path: "/login",
+        element: <>Login Page</>,
+      },
+      {
+        path: "/register",
+        element: <>Register Page</>,
+      },
+      {
+        path: "/carts",
+        element: <>Carts Page</>,
+      },
+      {
+        path: "/checkout",
+        element: <>Checkout Page</>,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: (
+      <>
+        <h1>Admin Layout</h1>
+        <Outlet></Outlet>
+      </>
+    ),
+    children: [
+      {
+        path: "/admin",
+        element: <>Admin Home</>,
+      },
+      {
+        path: "customers",
+        element: <>Customer Page</>,
+      },
+      {
+        path: "customers/:id",
+        element: <>Customer Details Page</>,
+      },
+      {
+        path: "orders",
+        element: <>Orders Page</>,
+      },
+      {
+        path: "orders/:id",
+        element: <>Order Details Page</>,
+      },
+      {
+        path: "products",
+        element: <>Products Page</>,
+      },
+      {
+        path: "products/:id",
+        element: <>Product Page</>,
+      },
+      {
+        path: "add-product",
+        element: <>Add Product Page</>,
       },
     ],
   },
