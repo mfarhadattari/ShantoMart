86096a11eff279b4d5ae6bdda42ab38151e278a3
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-07-07 15:51:11 +0600

    Load products from json file and display on ui using products card

--- src/pages/Products/ProductsPage.jsx
+++ src/pages/Products/ProductsPage.jsx
@@ -1,7 +1,18 @@
 import Cover from "../../components/Cover";
 import banner from "../../assets/Banners/banner1.jpg";
+import { useQuery } from "react-query";
+import Loaders from "../../components/Loaders";
+import ProductCard from "../../components/ProductCard";
 
 const ProductsPage = () => {
+  const { data: products = [], isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await fetch("/data/products.json");
+      return await res.json();
+    },
+  });
+
   return (
     <main>
       <Cover
@@ -9,6 +20,19 @@ const ProductsPage = () => {
         subheading="Get your favorite products"
         backgroundURL={banner}
       />
+      <section>
+        {isLoading ? (
+          <div className="h-[300px]">
+            <Loaders />
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-20 gap-5 p-5">
+            {products.map((product) => (
+              <ProductCard key={product._id} productInfo={product} />
+            ))}
+          </div>
+        )}
+      </section>
     </main>
   );
 };
